8d6f16b975882bdb8668da38bb556d82a1f2675a
J. Lewis <6710419+lewxdev@users.noreply.github.com>
2024-08-21 19:49:00 -0400

    feat: hardware upgrade and dynamic motd

--- run.sh
+++ run.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 TIME_TO_START=15 # seconds
-RAM_FOR_SYSTEM=1536 # 1.5 GiB
+RAM_FOR_SYSTEM=2048 # 2 GiB
 
 trap 'echo; echo "exiting gracefully..."; exit 1' INT
 
 while true; do
-  RAM_USAGE=$(( $(free -m | awk '/^Mem:/{print $2}') - RAM_FOR_SYSTEM ))
+  RAM_USAGE=$(($(free -m | awk '/^Mem:/{print $2}') - RAM_FOR_SYSTEM))
+  HR_RAM_USAGE=$(awk "BEGIN {printf \"%.1f\", $RAM_USAGE/1000}")
   echo
-  echo "allocating ${RAM_USAGE}MiB RAM to the server"
+  echo "allocating ${HR_RAM_USAGE}GiB RAM to the server"
+  echo "updating server.properties"
+  sed -i -E "s/(motd=.*)([0-9]+\.[0-9]+) GB/\1$HR_RAM_USAGE GB/" server.properties
   java -Xmx${RAM_USAGE}M -Xms${RAM_USAGE}M -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=8M -XX:G1HeapWastePercent=5 -XX:G1MaxNewSizePercent=40 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1NewSizePercent=30 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -XX:MaxGCPauseMillis=200 -XX:MaxTenuringThreshold=1 -XX:SurvivorRatio=32 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar server.jar --nogui
 
   echo
